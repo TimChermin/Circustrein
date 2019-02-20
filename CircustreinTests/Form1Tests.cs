09c661002e054cc3eac4b9d2719816e2c48abf76
Tim Chermin <tim.chermin@student.fontys.nl>
2019-02-20 12:03:32 +0100

    Unit tests updated

--- CircustreinTests/Form1Tests.cs
+++ CircustreinTests/Form1Tests.cs
@@ -12,13 +12,15 @@ namespace Circustrein.Tests
     public class Form1Tests
     {
 
-        Train train = new Train();
+        Train train;
 
         //adds the first Animals for testing (before everything else)
         [TestInitialize]
-        public void AddStartingAnimals()
+        public void CreateList()
         {
-            for (int i = 0; i <= 15; i++)
+            train = new Train();
+
+            /*for (int i = 0; i <= 15; i++)
             {
                 if (i > 8 & i < 11)
                 {
@@ -37,19 +39,31 @@ namespace Circustrein.Tests
                     train.AddAnimal("Herbivore", "Medium");
                 }
             }
+            */
             train.AddAnimalToWagons();
         }
 
+        //Should_ExpectedBehavior_When_StateUnderTest
 
-        //check for multiple carnivores in the wagons (more than 1 == false)
+
+        //Check for multiple carnivores in the wagons (more than 1 == false)
         [TestMethod()]
-        public void buttonAddAnimal_ClickTest()
+        public void Should_NotAddCarn_When_TheWagonAlreadyHasAnCarn()
         {
+            //Arrange
+            train.AddAnimal("Carnivore", "Small");
+            train.AddAnimal("Carnivore", "Small");
+            train.AddAnimal("Carnivore", "Small");
+            train.AddAnimal("Carnivore", "Small");
+            train.AddAnimal("Carnivore", "Small");
+            train.AddAnimalToWagons();
+
+            //Act
             foreach (Wagon wagon in train.LoadWagons())
             {
                 int carnivoresInWagon = 0;
 
-                
+
                 foreach (Animal animal in wagon.Animals)
                 {
                     if (animal.FoodType == "Carnivore")
@@ -58,20 +72,95 @@ namespace Circustrein.Tests
                     }
                 }
 
+                //Assert
                 Assert.IsFalse(carnivoresInWagon > 1);
             }
         }
 
-        //check if the wagons weigh less than 10
+        //Should_ExpectedBehavior_When_StateUnderTest
+
+        //Check if the wagons weigh less than 10
         [TestMethod()]
-        public void wagonWeight()
+        public void Should_NotGoOverTheMaxWagonWeight_When_AddingAnimals()
         {
+            //Arrange
+            train.AddAnimal("Herbivore", "Big");
+            train.AddAnimal("Herbivore", "Big");
+            train.AddAnimal("Herbivore", "Big");
+            train.AddAnimal("Herbivore", "Big");
+            
+            train.AddAnimalToWagons();
+
+            train.AddAnimal("Carnivore", "Medium");
+            train.AddAnimal("Herbivore", "Big");
+            train.AddAnimal("Carnivore", "Medium");
+            train.AddAnimal("Carnivore", "Medium");
+            train.AddAnimal("Carnivore", "Medium");
+            train.AddAnimal("Carnivore", "Medium");
+            train.AddAnimalToWagons();
+
+            //Act
             foreach (Wagon wagon in train.LoadWagons())
             {
+                //Assert
                 Assert.IsTrue(wagon.Weight <= 10);
             }
+
+            //Act
+            int wagonCount = 0;
+            foreach (Wagon wagon in train.LoadWagons())
+            {
+                wagonCount++;
+            }
+            //Assert
+            Assert.IsTrue(wagonCount == 5);
+        }
+
+        [TestMethod()]
+        public void Should_NotAddCarnToWagon_When_ItHasAnSmallerOrTheSameSizeHerb()
+        {
+            //Arrange
+            train.AddAnimal("Herbivore", "Small");
+            train.AddAnimal("Herbivore", "Small");
+            train.AddAnimal("Herbivore", "Small");
+            train.AddAnimal("Herbivore", "Small");
+            train.AddAnimal("Herbivore", "Small");
+
+            train.AddAnimal("Carnivore", "Small");
+            train.AddAnimal("Carnivore", "Small");
+            train.AddAnimal("Carnivore", "Small");
+            train.AddAnimal("Carnivore", "Small");
+            train.AddAnimal("Carnivore", "Small");
+            train.AddAnimalToWagons();
+
+            //Act
+            int wagonCount = 0;
+            foreach (Wagon wagon in train.LoadWagons())
+            {
+                wagonCount++;
+            }
+
+            //Assert
+            Assert.IsTrue(wagonCount == 6);
         }
 
 
+        [TestMethod()]
+        public void Should_NotAddCarnToWagon_When_ItHasAnSmall()
+        {
+            //Arrange
+            train.AddAnimal("Herbivore", "Small");
+            train.AddAnimal("Herbivore", "Small");
+            train.AddAnimal("Herbivore", "Small");
+            train.AddAnimal("Herbivore", "Small");
+            train.AddAnimal("Herbivore", "Small");
+
+            train.AddAnimal("Carnivore", "Small");
+            train.AddAnimal("Carnivore", "Small");
+            train.AddAnimal("Carnivore", "Small");
+            train.AddAnimal("Carnivore", "Small");
+            train.AddAnimal("Carnivore", "Small");
+            train.AddAnimalToWagons();
+        }
     }
 }
